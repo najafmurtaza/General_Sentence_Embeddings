b098d91d36010a28eee4cf55029fc24de02d6873
Najaf Murtaza <16462407+najafmurtaza@users.noreply.github.com>
2020-08-16 18:26:34 +0500

    Initial commit

---
+++ hf_emb_pytorch.py
@@ -0,0 +1,35 @@
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0]
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    
+    return sum_embeddings / sum_mask
+
+
+sentences = [
+    'This is an excellent movie',
+    'The move was fantastic I like it',
+    'Its a fantastic series',
+    'It is a Wonderful movie',
+    'I did not like the movie',
+    'I will not recommend',
+    'The acting is pathetic']
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+model = AutoModel.from_pretrained("distilbert-base-uncased")
+model.eval()
+
+# max_length depends on choosen network, it can't be greater than the network's max_length.
+# max_length=128(words) is used most of the time.
+encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=128, return_tensors='pt')
+
+with torch.no_grad():
+    model_output = model(**encoded_input)
+    print(model_output[0].shape) # (Total_sentences, tokens, tokens_dim)
+    
+sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+print(sentence_embeddings.shape) # (Total_sentences, tokens_dim)
---
+++ hf_emb_tensorflow.py
@@ -0,0 +1,45 @@
+from transformers import AutoTokenizer, TFAutoModel
+import tensorflow
+
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0]
+    
+    input_mask_expanded = tensorflow.expand_dims(attention_mask, axis=2)
+    input_mask_expanded = tensorflow.broadcast_to(input_mask_expanded, token_embeddings.shape)
+    input_mask_expanded = tensorflow.cast(input_mask_expanded, tensorflow.float32)
+    
+    sum_embeddings = tensorflow.math.reduce_sum(token_embeddings * input_mask_expanded, axis=1)
+    summed_input_mask_expanded = tensorflow.math.reduce_sum(input_mask_expanded, axis=1)
+    
+    # we want to clip only values which are less than min (similar to pytorch code).
+    # As tensorflow.clip_by_value expects max values as well, we'll set this value to max of array.
+    # As a result, returned tensor would be clipped values which are less than min not max
+    clip_value_min = 1e-9
+    clip_value_max = tensorflow.reduce_max(summed_input_mask_expanded, keepdims=True, axis=1)
+    sum_mask = tensorflow.clip_by_value(summed_input_mask_expanded, clip_value_min=clip_value_min,
+                                       clip_value_max=clip_value_max)
+    
+    return sum_embeddings / sum_mask
+
+
+sentences = [
+    'This is an excellent movie',
+    'The move was fantastic I like it',
+    'Its a fantastic series',
+    'It is a Wonderful movie',
+    'I did not like the movie',
+    'I will not recommend',
+    'The acting is pathetic']
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+model = TFAutoModel.from_pretrained("distilbert-base-uncased")
+
+# max_length depends on choosen network, it can't be greater than the network's max_length.
+# max_length=128(words) is used most of the time.
+encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=128, return_tensors='tf')
+
+model_output = model(encoded_input)
+print(model_output[0].shape) # (Total_sentences, tokens, tokens_dim)
+    
+sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+print(sentence_embeddings.shape) #(Total_sentences, tokens_dim)
